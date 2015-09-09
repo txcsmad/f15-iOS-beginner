40bd2f2a5c911b7d614df6db092d9c1d8fdf7ae8
Nick Walker <nick@nickwalker.us>
2015-09-08 20:14:09 -0500

    Fixed word I don't like

--- WordCollage/WordCollage/ViewController.swift
+++ WordCollage/WordCollage/ViewController.swift
@@ -7,7 +7,7 @@ This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAl
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
