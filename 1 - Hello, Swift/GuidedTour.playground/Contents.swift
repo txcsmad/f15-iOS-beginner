a78846e048e7da79f64c59b87e1fc74807a54aa8
Nick Walker <nick@nickwalker.us>
2015-09-15 13:55:39 -0500

    Remove some whitespace
    Add experiments back in

--- 1 - Hello, Swift/GuidedTour.playground/Contents.swift
+++ 1 - Hello, Swift/GuidedTour.playground/Contents.swift
@@ -20,12 +20,15 @@ let implicitInteger = 70
 let implicitDouble = 70.0
 let explicitDouble: Double = 70
 
+//: *EXPERIMENT:* Create a constant with an explicit type of Float and a value of 4.
 
 //: Values are never implicitly converted to another type. If you need to convert a value to a different type, explicitly make an instance of the desired type.
 
 let label = "The width is "
 let width = 94
 let widthLabel = label + String(width)
+
+//: *EXPERIMENT:* Try removing the conversion to String from the last line. What error do you get?
 
 //: There’s an even simpler way to include values in strings: Write the value in parentheses, and write a backslash (`\`) before the parentheses. For example:
 
@@ -34,9 +37,10 @@ let oranges = 5
 let appleSummary = "I have \(apples) apples."
 let fruitSummary = "I have \(apples + oranges) pieces of fruit."
 
+//: *EXPERIMENT:* Use `\()` to include a floating-point calculation in a string and to include someone’s name in a greeting.
+
 //: ### Data structures
 //: Swift provides built in `Array` and `Ditionary` types.
-
 var shoppingList = ["catfish", "water", "tulips", "blue paint"]
 shoppingList[1] = "bottle of water"
  
@@ -50,7 +54,6 @@ occupations["Jayne"] = "Public Relations"
 
 let emptyArray = [String]()
 let emptyDictionary = [String: Float]()
-
 //: If type information can be inferred, you can write an empty array as `[]` and an empty dictionary as `[:]`—for example, when you set a new value for a variable or pass an argument to a function.
 
 shoppingList = []
@@ -82,6 +85,7 @@ if let name = optionalName {
     greeting = "Hello, \(name)"
 }
 
+//: *EXPERIMENT:* Change `optionalName` to `nil`. What greeting do you get? Add an `else` clause that sets a different greeting if `optionalName` is `nil`.
 
 //: If the optional value is `nil`, the conditional is `false` and the code in braces is skipped. Otherwise, the optional value is unwrapped and assigned to the constant after `let`, which makes the unwrapped value available inside the block of code.
 //: Switches support any kind of data and a wide variety of comparison operations—they aren’t limited to integers and tests for equality.
@@ -97,6 +101,8 @@ case let x where x.hasSuffix("pepper"):
 default:
     let vegetableComment = "Everything tastes good in soup."
 }
+
+//: *EXPERIMENT:* Try removing the default case. What error do you get?
 
 //: Notice how `let` can be used in a pattern to assign the value that matched that part of a pattern to a constant.
 //: After executing the code inside the switch case that matched, the program exits from the switch statement. Execution doesn’t continue to the next case, so there is no need to explicitly break out of the switch at the end of each case’s code.
@@ -117,9 +123,10 @@ for (kind, numbers) in interestingNumbers {
 }
 println(largest)
 
+//: *EXPERIMENT:* Add another variable to keep track of which kind of number was the largest, as well as what that largest number was.
+
 
 //: Use `while` to repeat a block of code until a condition changes. The condition of a loop can be at the end instead, ensuring that the loop is run at least once.
-
 var n = 2
 while n < 100 {
     n = n * 2
@@ -150,18 +157,14 @@ println(secondForLoop)
 
 //: ### Functions and Closures
 //: Use `func` to declare a function. Call a function by following its name with a list of arguments in parentheses. Use `->` to separate the parameter names and types from the function’s return type.
-
-
 func greet(name: String, day: String) -> String {
     return "Hello \(name), today is \(day)."
 }
 greet("Bob", "Tuesday")
-    	
-    
+
+//: *EXPERIMENT:* Remove the day parameter. Add a parameter to include today’s lunch special in the greeting.
 
 //: Use a tuple to make a compound value—for example, to return multiple values from a function. The elements of a tuple can be referred to either by name or by number.
-
-
 func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
     var min = scores[0]
     var max = scores[0]
@@ -183,8 +186,6 @@ println(statistics.sum)
 println(statistics.2)
 
 //: Functions can also take a variable number of arguments, collecting them into an array.
-
-
 func sumOf(numbers: Int...) -> Int {
     var sum = 0
     for number in numbers {
@@ -195,8 +196,9 @@ func sumOf(numbers: Int...) -> Int {
 sumOf()
 sumOf(42, 597, 12)
 
-//: Functions can be nested. Nested functions have access to variables that were declared in the outer function. You can use nested functions to organize the code in a function that is long or complex.
+//: *EXPERIMENT:* Write a function that calculates the average of its arguments.
 
+//: Functions can be nested. Nested functions have access to variables that were declared in the outer function. You can use nested functions to organize the code in a function that is long or complex.
 func returnFifteen() -> Int {
     var y = 10
     func add() {
@@ -208,7 +210,6 @@ func returnFifteen() -> Int {
 returnFifteen()
 
 //: Functions are a first-class type. This means that a function can return another function as its value.
-
 func makeIncrementer(incrementsBy: Int) -> (Int -> Int) {
     func incrementor(number: Int) -> Int {
         return incrementsBy + number
@@ -220,7 +221,6 @@ increment(7)
 
 
 //: A function can take another function as one of its arguments.
-
 func hasAnyMatches(list: [Int], condition: Int -> Bool) -> Bool {
     for item in list {
         if condition(item) {
@@ -235,30 +235,21 @@ func lessThanTen(number: Int) -> Bool {
 var numbers = [20, 19, 7, 12]
 hasAnyMatches(numbers, lessThanTen)
 
-
 //: Functions are actually a special case of closures: blocks of code that can be called later. The code in a closure has access to things like variables and functions that were available in the scope where the closure was created, even if the closure is in a different scope when it is executed—you saw an example of this already with nested functions. You can write a closure without a name by surrounding code with braces (`{}`). Use `in` to separate the arguments and return type from the body.
-
-
-
 numbers.map({
     (number: Int) -> Int in
     let result = 3 * number
     return result
 })
-    	
-    
+
+//: *EXPERIMENT:* Rewrite the closure to return zero for all odd numbers.
 
 //: You have several options for writing closures more concisely. When a closure’s type is already known, such as the callback for a delegate, you can omit the type of its parameters, its return type, or both. Single statement closures implicitly return the value of their only statement.
-
-
-
 let mappedNumbers = numbers.map({ number in 3 * number })
 println(mappedNumbers)
 
 
 //: You can refer to parameters by number instead of by name—this approach is especially useful in very short closures. A closure passed as the last argument to a function can appear immediately after the parentheses.
-
-
 let sortedNumbers = sorted(numbers) { $0 > $1 }
 println(sortedNumbers)
 
@@ -266,9 +257,6 @@ println(sortedNumbers)
 
 //: ### Objects and Classes
 //: Use `class` followed by the class’s name to create a class. A property declaration in a class is written the same way as a constant or variable declaration, except that it is in the context of a class. Likewise, method and function declarations are written the same way.
-
-
-
 class Shape {
     var numberOfSides = 0
     func simpleDescription() -> String {
@@ -276,21 +264,14 @@ class Shape {
     }
 }
 
+//: *EXPERIMENT:* Add a constant property with let, and add another method that takes an argument.
 
 //: Create an instance of a class by putting parentheses after the class name. Use dot syntax to access the properties and methods of the instance.
-
-
 var shape = Shape()
 shape.numberOfSides = 7
 var shapeDescription = shape.simpleDescription()
 
-
-
-
 //: This version of the `Shape` class is missing something important: an initializer to set up the class when an instance is created. Use `init` to create one.
-
-
-
 class NamedShape {
     var numberOfSides: Int = 0
     var name: String
@@ -304,9 +285,6 @@ class NamedShape {
     }
 }
 
-
-
-
 //: Notice how `self` is used to distinguish the `name` property from the `name` argument to the initializer. The arguments to the initializer are passed like a function call when you create an instance of the class. Every property needs a value assigned—either in its declaration (as with `numberOfSides`) or in the initializer (as with `name`).
 
 //: Use `deinit` to create a deinitializer if you need to perform some cleanup before the object is deallocated.
@@ -314,9 +292,6 @@ class NamedShape {
 //: Subclasses include their superclass name after their class name, separated by a colon. There is no requirement for classes to subclass any standard root class, so you can include or omit a superclass as needed.
 
 //: Methods on a subclass that override the superclass’s implementation are marked with `override`—overriding a method by accident, without `override`, is detected by the compiler as an error. The compiler also detects methods with `override` that don’t actually override any method in the superclass.
-
-
-
 class Square: NamedShape {
     var sideLength: Double
     
@@ -338,18 +313,9 @@ let test = Square(sideLength: 5.2, name: "my test square")
 test.area()
 test.simpleDescription()
 
-
-
-
-    
-//: Make another subclass of `NamedShape` called `Circle` that takes a radius and a name as arguments to its initializer. Implement an `area()` and a `simpleDescription()` method on the `Circle` class.
-    	
-    
+//: *EXPERIMENT:* Make another subclass of `NamedShape` called `Circle` that takes a radius and a name as arguments to its initializer. Implement an `area()` and a `simpleDescription()` method on the `Circle` class.
 
 //: In addition to simple properties that are stored, properties can have a getter and a setter.
-
-
-
 class EquilateralTriangle: NamedShape {
     var sideLength: Double = 0.0
     
@@ -378,20 +344,14 @@ triangle.perimeter = 9.9
 println(triangle.sideLength)
 
 
-
-
 //: In the setter for `perimeter`, the new value has the implicit name `newValue`. You can provide an explicit name in parentheses after `set`.
 
 /*: Notice that the initializer for the `EquilateralTriangle` class has three different steps:
-    * Setting the value of properties that the subclass declares.
-    * Calling the superclass’s initializer.
-    * Changing the value of properties defined by the superclass. Any additional setup work that uses methods, getters, or setters can also be done at this point.
-
+* Setting the value of properties that the subclass declares.
+* Calling the superclass’s initializer.
+* Changing the value of properties defined by the superclass. Any additional setup work that uses methods, getters, or setters can also be done at this point.
 */
 //: If you don’t need to compute the property but still need to provide code that is run before and after setting a new value, use `willSet` and `didSet`. For example, the class below ensures that the side length of its triangle is always the same as the side length of its square.
-
-
-
 class TriangleAndSquare {
     var triangle: EquilateralTriangle {
         willSet {
@@ -414,11 +374,7 @@ println(triangleAndSquare.triangle.sideLength)
 triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
 println(triangleAndSquare.triangle.sideLength)
 
-
 //: Methods on classes have one important difference from functions. Parameter names in functions are used only within the function, but parameters names in methods are also used when you call the method (except for the first parameter). By default, a method has the same name for its parameters when you call it and within the method itself. You can specify a second name, which is used inside the method.
-
-
-
 class Counter {
     var count: Int = 0
     func incrementBy(amount: Int, numberOfTimes times: Int) {
@@ -428,23 +384,13 @@ class Counter {
 var counter = Counter()
 counter.incrementBy(2, numberOfTimes: 7)
 
-
-
-
 //: When working with optional values, you can write `?` before operations like methods, properties, and subscripting. If the value before the `?` is `nil`, everything after the `?` is ignored and the value of the whole expression is `nil`. Otherwise, the optional value is unwrapped, and everything after the `?` acts on the unwrapped value. In both cases, the value of the whole expression is an optional value.
-
-
-
 let optionalSquare: Square? = Square(sideLength: 2.5, name: "optional square")
 let sideLength = optionalSquare?.sideLength
 
 
-
 //: ### Enumerations and Structures
 //: Use `enum` to create an enumeration. Like classes and all other named types, enumerations can have methods associated with them.
-
-
-
 enum Rank: Int {
     case Ace = 1
     case Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten
@@ -469,15 +415,12 @@ let aceRawValue = ace.rawValue
 
 
 //: In the example above, the raw-value type of the enumeration is `Int`, so you only have to specify the first raw value. The rest of the raw values are assigned in order. You can also use strings or floating-point numbers as the raw type of an enumeration. Use the `rawValue` property to access the raw value of an enumeration member.
-
-
 if let convertedRank = Rank(rawValue: 3) {
     let threeDescription = convertedRank.simpleDescription()
 }
 
 
 //: The member values of an enumeration are actual values, not just another way of writing their raw values. In fact, in cases where there isn’t a meaningful raw value, you don’t have to provide one.
-
 enum Suit {
     case Spades, Hearts, Diamonds, Clubs
     func simpleDescription() -> String {
@@ -495,14 +438,12 @@ enum Suit {
 }
 let hearts = Suit.Hearts
 let heartsDescription = hearts.simpleDescription()
-    
+
+//: *EXPERIMENT:* Add a color() method to Suit that returns “black” for spades and clubs, and returns “red” for hearts and diamonds.
 
 //: Notice the two ways that the `Hearts` member of the enumeration is referred to above: When assigning a value to the `hearts` constant, the enumeration member `Suit.Hearts` is referred to by its full name because the constant doesn’t have an explicit type specified. Inside the switch, the enumeration member is referred to by the abbreviated form `.Hearts` because the value of `self` is already known to be a suit. You can use the abbreviated form anytime the value’s type is already known.
 
 //: Use `struct` to create a structure. Structures support many of the same behaviors as classes, including methods and initializers. One of the most important differences between structures and classes is that structures are always copied when they are passed around in your code, but classes are passed by reference.
-
-
-
 struct Card {
     var rank: Rank
     var suit: Suit
@@ -513,17 +454,11 @@ struct Card {
 let threeOfSpades = Card(rank: .Three, suit: .Spades)
 let threeOfSpadesDescription = threeOfSpades.simpleDescription()
 
-    
-//: Add a method to `Card` that creates a full deck of cards, with one card of each combination of rank and suit.
-    	
-    
+//: *EXPERIMENT:* Add a method to Card that creates a full deck of cards, with one card of each combination of rank and suit.
 
 //: An instance of an enumeration member can have values associated with the instance. Instances of the same enumeration member can have different values associated with them. You provide the associated values when you create the instance. Associated values and raw values are different: The raw value of an enumeration member is the same for all of its instances, and you provide the raw value when you define the enumeration.
 
 //: For example, consider the case of requesting the sunrise and sunset time from a server. The server either responds with the information or it responds with some error information.
-
-
-
 enum ServerResponse {
     case Result(String, String)
     case Error(String)
@@ -539,14 +474,13 @@ case let .Error(error):
     let serverResponse = "Failure...  \(error)"
 }
 
+//: *EXPERIMENT:* Add a third case to ServerResponse and to the switch.
+
 //: Notice how the sunrise and sunset times are extracted from the `ServerResponse` value as part of matching the value against the switch cases.
 
 
 //: ### Protocols and Extensions
 //: Use `protocol` to declare a protocol.
-
-
-
 protocol ExampleProtocol {
     var simpleDescription: String { get }
     mutating func adjust()
@@ -554,9 +488,6 @@ protocol ExampleProtocol {
 
 
 //: Classes, enumerations, and structs can all adopt protocols.
-
-
-
 class SimpleClass: ExampleProtocol {
     var simpleDescription: String = "A very simple class."
     var anotherProperty: Int = 69105
@@ -578,10 +509,11 @@ var b = SimpleStructure()
 b.adjust()
 let bDescription = b.simpleDescription
 
+//: *EXPERIMENT:* Write an enumeration that conforms to this protocol.
+
 //: Notice the use of the `mutating` keyword in the declaration of `SimpleStructure` to mark a method that modifies the structure. The declaration of `SimpleClass` doesn’t need any of its methods marked as mutating because methods on a class can always modify the class.
 
 //: Use `extension` to add functionality to an existing type, such as new methods and computed properties. You can use an extension to add protocol conformance to a type that is declared elsewhere, or even to a type that you imported from a library or framework.
-
 extension Int: ExampleProtocol {
     var simpleDescription: String {
         return "The number \(self)"
@@ -592,20 +524,18 @@ extension Int: ExampleProtocol {
 }
 println(7.simpleDescription)
 
+//: *EXPERIMENT:* Write an extension for the Double type that adds an absoluteValue property.
 
 //: You can use a protocol name just like any other named type—for example, to create a collection of objects that have different types but that all conform to a single protocol. When you work with values whose type is a protocol type, methods outside the protocol definition are not available.
-
 let protocolValue: ExampleProtocol = a
 println(protocolValue.simpleDescription)
 // println(protocolValue.anotherProperty)  // Uncomment to see the error
-
 
 //: Even though the variable `protocolValue` has a runtime type of `SimpleClass`, the compiler treats it as the given type of `ExampleProtocol`. This means that you can’t accidentally access methods or properties that the class implements in addition to its protocol conformance.
 
 
 //: ### Generics
 //: Write a name inside angle brackets to make a generic function or type.
-
 func repeat<Item>(item: Item, times: Int) -> [Item] {
     var result = [Item]()
     for i in 0..<times {
@@ -615,9 +545,7 @@ func repeat<Item>(item: Item, times: Int) -> [Item] {
 }
 repeat("knock", 4)
 
-
 //: You can make generic forms of functions and methods, as well as classes, enumerations, and structures.
-
 enum OptionalValue<T> {
     case None
     case Some(T)
@@ -626,7 +554,6 @@ var possibleInteger: OptionalValue<Int> = .None
 possibleInteger = .Some(100)
 
 //: Use `where` after the type name to specify a list of requirements—for example, to require the type to implement a protocol, to require two types to be the same, or to require a class to have a particular superclass.
-
 func anyCommonElements <T, U where T: SequenceType, U: SequenceType, T.Generator.Element: Equatable, T.Generator.Element == U.Generator.Element> (lhs: T, rhs: U) -> Bool {
     for lhsItem in lhs {
         for rhsItem in rhs {
@@ -639,5 +566,6 @@ func anyCommonElements <T, U where T: SequenceType, U: SequenceType, T.Generator
 }
 anyCommonElements([1, 2, 3], [3])
 
-    
+//: *EXPERIMENT:* Modify the anyCommonElements(_:_:) function to make a function that returns an array of the elements that any two sequences have in common.
+
 //: In the simple cases, you can omit `where` and simply write the protocol or class name after a colon. Writing `<T: Equatable>` is the same as writing `<T where T: Equatable>`.
