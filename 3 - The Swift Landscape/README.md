dbeb7d0e82db894221f28135ed7df1f2f32e36e5
Nick Walker <nick@nickwalker.us>
2015-09-29 22:10:47 -0500

    Add workshop 3 final state

--- 3 - The Swift Landscape/README.md
+++ 3 - The Swift Landscape/README.md
@@ -1,9 +1,14 @@
-## 2 - Connecting Things
+## 3 - The Swift Landscape
 
 September 29th, GDC 1.304, 7:00-8:30PM
 
 ### What We Did
 
+* Learned about the grand, all seeing view-controller that manages all of the views on our screen.
+* Made a button that iterated over all of the subviews and made every label's text red.
+* Made a button that removed a label each time it was pressed by calling the appropriate method on the label.
+* Made a button that added a subview to the view-controller's view
+* Made an IBAction that removed a button before a user was even done pressing it
 
 ### Exit Survey
 
--- 3 - The Swift Landscape/WordCollage/WordCollage/ViewController.swift
+++ 3 - The Swift Landscape/WordCollage/WordCollage/ViewController.swift
@@ -7,14 +7,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var coolLabel: UILabel!
 
     @IBAction func makeLabelRed(sender: UIButton) {
-        coolLabel.textColor = UIColor.redColor()
+        let subviews = view.subviews
+        subviews[0].removeFromSuperview()
+        for view in subviews {
+            let labelView = view as? UILabel
+            labelView?.textColor = UIColor.redColor()
+            // The above line is really just a more succint way of saying the below!
+
+            //if labelView != nil {
+            //   labelView!.textColor = UIColor.redColor()
+            //}
+        }
     }
+    @IBAction func didTouchDownGreenButton(sender: UIButton) {
+        //Let's do something nasty and remove the view out
+        //from underneathth the user's finger
+        sender.removeFromSuperview()
+    }
+
     @IBAction func makeLabelGreen (sender: UIButton) {
         coolLabel.textColor = UIColor.greenColor()
     }
 
     @IBAction func makeLabelPurple(sender: AnyObject) {
-        coolLabel.textColor = UIColor.purpleColor()
+        //Make the view.
+        let newView = UIView(frame: CGRect(x: 400, y: 10, width: 100, height: 100))
+        newView.backgroundColor = UIColor.purpleColor()
+        view.addSubview(newView)
+        //We've added a view!
     }
 }
 
